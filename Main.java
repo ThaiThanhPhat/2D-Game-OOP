8b43151c8e90620dd8b307be909fd1361d986a99
ThaiThanhPhat <96247504+ThaiThanhPhat@users.noreply.github.com>
2023-03-09 23:57:34 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Main;
+import javax.swing.JFrame;
+
+import Main.GamePanel;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.pack();
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
